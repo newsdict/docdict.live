54fbcf452ceb4209957a8e498c67396669370230
Yusuke Abe <yusuke@ybj.jp>
2021-03-07 09:51:11 +0900

    Remove react-admin as it cannot be posted from the server side.

--- pages/admin/index.tsx
+++
@@ -1,9 +0,0 @@
-import dynamic from "next/dynamic"
-
-const ReactAdmin = dynamic(() => import("../../components/admin/ReactAdmin"), {
-  ssr: false,
-})
-
-const HomePage = () => <ReactAdmin />
-
-export default HomePage
